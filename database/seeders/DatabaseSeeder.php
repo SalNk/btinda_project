aed9fe74aa80fb7e6acef39c571d55fa0c84bff3
Sal Aime <salemnk02@gmail.com>
2025-02-10 22:59:05 +0100

    add faker user admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +15,13 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory(30)->create();
+
+        User::create([
+            'name' => 'Admin',
+            'role' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('admin123')
+        ]);
 
         $this->call([
             SellerSeeder::class,
